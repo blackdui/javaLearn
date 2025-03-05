3d4465ec95ce39ac1b08586131af03d600dd75d8
blackdui <2501862946@qq.com>
2025-03-05 18:02:03 +0800

    add ignore

--- module2/src/com/blackdui/output/Demo1.java
+++ module2/src/com/blackdui/output/Demo1.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 
 public class Demo1 {
     public static void main(String[] args) throws IOException {
-        FileOutputStream fos = new FileOutputStream("1.txt");
+        FileOutputStream fos = new FileOutputStream("1.txt", true);
 //        fos.write(100);
-        String s = "marisa and reimu";
+        String s = "\n marisa and reimu";
         byte[] bytes = s.getBytes();
         fos.write(bytes);
         fos.close();
--- module2/src/com/blackdui/output/Demo2.java
+++ module2/src/com/blackdui/output/Demo2.java
@@ -1,7 +1,18 @@
 package com.blackdui.output;
 
-public class Demo2 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class Demo2 {
+    public static void main(String[] args) throws IOException {
+//        String s = "marsia";
+//        byte[] bytes = s.getBytes();
+
+        File file1 = new File("1.txt");
+        FileOutputStream fos = new FileOutputStream(file1, true);
+//        fos.write(bytes, 0, 3);
+        fos.write("\nreimu".getBytes());
+        fos.close();
     }
 }
